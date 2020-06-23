540d6ccd3567d82b7c10a68b41bc22cc1f813895
Amartya Mondal <atm1504.in@gmail.com>
2020-06-24 02:10:17 +0530

    feat: Revamp the sample code according to my need

--- lib/screens/admin.dart
+++ lib/screens/admin.dart
@@ -1,9 +1,8 @@
 import 'package:atmmartadmin/db/brand.dart';
 import 'package:atmmartadmin/db/category.dart';
+import 'package:atmmartadmin/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-//import '../db/category.dart';
-//import '../db/brand.dart';
 
 enum Page { dashboard, manage }
 
@@ -15,6 +14,8 @@ class Admin extends StatefulWidget {
 class _AdminState extends State<Admin> {
   Page _selectedPage = Page.dashboard;
   MaterialColor active = Colors.red;
+  MaterialColor selected = Colors.blue;
+  var notSelected = Colors.white;
   MaterialColor notActive = Colors.grey;
   TextEditingController categoryController = TextEditingController();
   TextEditingController brandController = TextEditingController();
@@ -31,6 +32,9 @@ class _AdminState extends State<Admin> {
             children: <Widget>[
               Expanded(
                   child: FlatButton.icon(
+                      color: _selectedPage == Page.dashboard
+                          ? selected
+                          : notSelected,
                       onPressed: () {
                         setState(() => _selectedPage = Page.dashboard);
                       },
@@ -43,6 +47,8 @@ class _AdminState extends State<Admin> {
                       label: Text('Dashboard'))),
               Expanded(
                   child: FlatButton.icon(
+                      color:
+                          _selectedPage == Page.manage ? selected : notSelected,
                       onPressed: () {
                         setState(() => _selectedPage = Page.manage);
                       },
@@ -55,7 +61,7 @@ class _AdminState extends State<Admin> {
             ],
           ),
           elevation: 0.0,
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.orange[900],
         ),
         body: _loadScreen());
   }
@@ -63,171 +69,193 @@ class _AdminState extends State<Admin> {
   Widget _loadScreen() {
     switch (_selectedPage) {
       case Page.dashboard:
-        return Column(
-          children: <Widget>[
-            ListTile(
-              subtitle: FlatButton.icon(
-                onPressed: null,
-                icon: Icon(
-                  Icons.attach_money,
-                  size: 30.0,
-                  color: Colors.green,
+        return Container(
+          color: Colors.orange[100],
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                subtitle: FlatButton.icon(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.attach_money,
+                    size: 30.0,
+                    color: Colors.green,
+                  ),
+                  label: Text('12,000',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30.0, color: Colors.green)),
                 ),
-                label: Text('12,000',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 30.0, color: Colors.green)),
+                title: Text(
+                  'Revenue',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 24.0, color: Colors.black87),
+                ),
               ),
-              title: Text(
-                'Revenue',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 24.0, color: Colors.grey),
+              Expanded(
+                child: GridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Card(
+                        child: ListTile(
+                            title: FlatButton.icon(
+                                onPressed: null,
+                                icon: Icon(Icons.people_outline),
+                                label: Text("Users")),
+                            subtitle: Text(
+                              '7',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: active, fontSize: 60.0),
+                            )),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Card(
+                        child: ListTile(
+                            title: FlatButton.icon(
+                                onPressed: null,
+                                icon: Icon(
+                                  Icons.category,
+                                  size: 15,
+                                ),
+                                label: Text("Categories")),
+                            subtitle: Text(
+                              '23',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: active, fontSize: 60.0),
+                            )),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(22.0),
+                      child: Card(
+                        child: ListTile(
+                            title: FlatButton.icon(
+                                onPressed: null,
+                                icon: Icon(Icons.track_changes),
+                                label: Text("Producs")),
+                            subtitle: Text(
+                              '120',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: active, fontSize: 60.0),
+                            )),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(22.0),
+                      child: Card(
+                        child: ListTile(
+                            title: FlatButton.icon(
+                                onPressed: null,
+                                icon: Icon(Icons.tag_faces),
+                                label: Text("Sold")),
+                            subtitle: Text(
+                              '13',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: active, fontSize: 60.0),
+                            )),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(22.0),
+                      child: Card(
+                        child: ListTile(
+                            title: FlatButton.icon(
+                                onPressed: null,
+                                icon: Icon(Icons.shopping_cart),
+                                label: Text("Orders")),
+                            subtitle: Text(
+                              '5',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: active, fontSize: 60.0),
+                            )),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(22.0),
+                      child: Card(
+                        child: ListTile(
+                            title: FlatButton.icon(
+                                onPressed: null,
+                                icon: Icon(Icons.close),
+                                label: Text("Return")),
+                            subtitle: Text(
+                              '0',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: active, fontSize: 60.0),
+                            )),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: GridView(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: Card(
-                      child: ListTile(
-                          title: FlatButton.icon(
-                              onPressed: null,
-                              icon: Icon(Icons.people_outline),
-                              label: Text("Users")),
-                          subtitle: Text(
-                            '7',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: active, fontSize: 60.0),
-                          )),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: Card(
-                      child: ListTile(
-                          title: FlatButton.icon(
-                              onPressed: null,
-                              icon: Icon(Icons.category),
-                              label: Text("Categories")),
-                          subtitle: Text(
-                            '23',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: active, fontSize: 60.0),
-                          )),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(22.0),
-                    child: Card(
-                      child: ListTile(
-                          title: FlatButton.icon(
-                              onPressed: null,
-                              icon: Icon(Icons.track_changes),
-                              label: Text("Producs")),
-                          subtitle: Text(
-                            '120',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: active, fontSize: 60.0),
-                          )),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(22.0),
-                    child: Card(
-                      child: ListTile(
-                          title: FlatButton.icon(
-                              onPressed: null,
-                              icon: Icon(Icons.tag_faces),
-                              label: Text("Sold")),
-                          subtitle: Text(
-                            '13',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: active, fontSize: 60.0),
-                          )),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(22.0),
-                    child: Card(
-                      child: ListTile(
-                          title: FlatButton.icon(
-                              onPressed: null,
-                              icon: Icon(Icons.shopping_cart),
-                              label: Text("Orders")),
-                          subtitle: Text(
-                            '5',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: active, fontSize: 60.0),
-                          )),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(22.0),
-                    child: Card(
-                      child: ListTile(
-                          title: FlatButton.icon(
-                              onPressed: null,
-                              icon: Icon(Icons.close),
-                              label: Text("Return")),
-                          subtitle: Text(
-                            '0',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: active, fontSize: 60.0),
-                          )),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         );
         break;
       case Page.manage:
-        return ListView(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.add),
-              title: Text("Add product"),
-              onTap: () {},
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.change_history),
-              title: Text("Products list"),
-              onTap: () {},
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.add_circle),
-              title: Text("Add category"),
-              onTap: () {
-                _categoryAlert();
-              },
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.category),
-              title: Text("Category list"),
-              onTap: () {},
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.add_circle_outline),
-              title: Text("Add brand"),
-              onTap: () {
-                _brandAlert();
-              },
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.library_books),
-              title: Text("brand list"),
-              onTap: () {},
-            ),
-            Divider(),
-          ],
+        return Container(
+          color: Colors.yellow[100],
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.add),
+                title: Text(
+                  "Add product",
+                  style: TextStyle(color: Colors.black),
+                ),
+                onTap: () {},
+              ),
+              Divider(
+                color: Colors.red,
+              ),
+              ListTile(
+                leading: Icon(Icons.change_history),
+                title: Text("Products list"),
+                onTap: () {},
+              ),
+              Divider(
+                color: Colors.red,
+              ),
+              ListTile(
+                leading: Icon(Icons.add_circle),
+                title: Text("Add category"),
+                onTap: () {
+                  _categoryAlert();
+                },
+              ),
+              Divider(
+                color: Colors.red,
+              ),
+              ListTile(
+                leading: Icon(Icons.category),
+                title: Text("Category list"),
+                onTap: () {},
+              ),
+              Divider(
+                color: Colors.red,
+              ),
+              ListTile(
+                leading: Icon(Icons.add_circle_outline),
+                title: Text("Add brand"),
+                onTap: () {
+                  _brandAlert();
+                },
+              ),
+              Divider(
+                color: Colors.red,
+              ),
+              ListTile(
+                leading: Icon(Icons.library_books),
+                title: Text("brand list"),
+                onTap: () {},
+              ),
+              Divider(),
+            ],
+          ),
         );
         break;
       default:
@@ -242,21 +270,24 @@ class _AdminState extends State<Admin> {
         child: TextFormField(
           controller: categoryController,
           validator: (value) {
-            if (value.isEmpty) {
-              return 'category cannot be empty';
+            if (value.length <= 0) {
+              return 'Category cannot be empty';
             }
           },
-          decoration: InputDecoration(hintText: "add category"),
+          decoration: InputDecoration(hintText: "Add category"),
         ),
       ),
       actions: <Widget>[
         FlatButton(
             onPressed: () {
-              if (categoryController.text != null) {
+              if (categoryController.text.length != 0) {
                 _categoryService.createCategory(categoryController.text);
+                showToast("Category created successfully");
+                Navigator.pop(context);
+              } else {
+                Navigator.pop(context);
+                showToast("Category field cannot be empty");
               }
-              Fluttertoast.showToast(msg: 'category created');
-              Navigator.pop(context);
             },
             child: Text('ADD')),
         FlatButton(
@@ -287,11 +318,14 @@ class _AdminState extends State<Admin> {
       actions: <Widget>[
         FlatButton(
             onPressed: () {
-              if (brandController.text != null) {
+              if (brandController.text.length > 0) {
                 _brandService.createBrand(brandController.text);
+                Navigator.pop(context);
+                showToast("Brand added successfully");
+              } else {
+                Navigator.pop(context);
+                showToast("Brand field cannot be empty");
               }
-              Fluttertoast.showToast(msg: 'brand added');
-              Navigator.pop(context);
             },
             child: Text('ADD')),
         FlatButton(
